1791fbf843043adc7a5449946d2c564e7446097f
Anjana Tissera <tisserapac@yahoo.com>
2023-06-13 15:47:36 +0700

    Completed section 5

--- Udemy_Angular_Material/fitness-tracker/src/app/material.module.ts
+++ Udemy_Angular_Material/fitness-tracker/src/app/material.module.ts
@@ -15,6 +15,9 @@ import {MatCardModule} from '@angular/material/card';
 import {MatSelectModule} from '@angular/material/select';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import {MatDialogModule} from '@angular/material/dialog';
+import {MatTableModule} from '@angular/material/table';
+import {MatSortModule} from '@angular/material/sort';
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 
 @NgModule({
@@ -34,7 +37,10 @@ import {MatDialogModule} from '@angular/material/dialog';
     MatCardModule,
     MatSelectModule,
     MatProgressSpinnerModule, 
-    MatDialogModule],
+    MatDialogModule, 
+    MatTableModule, 
+    MatSortModule,
+    MatPaginatorModule],
   exports: [
     MatButtonModule, 
     MatIconModule, 
@@ -51,7 +57,11 @@ import {MatDialogModule} from '@angular/material/dialog';
     MatCardModule,
     MatSelectModule,
     MatProgressSpinnerModule, 
-    MatDialogModule]
+    MatDialogModule,
+    MatTableModule,
+    MatSortModule,
+    MatPaginatorModule
+  ]
     
 })
 export class MaterialModule {}
--- Udemy_Angular_Material/fitness-tracker/src/app/training/past-training/past-training.component.ts
+++ Udemy_Angular_Material/fitness-tracker/src/app/training/past-training/past-training.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, AfterViewInit} from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
+import {MatPaginator} from '@angular/material/paginator';
+import {MatSort} from '@angular/material/sort';
+
+import { Exercise } from '../exercise.model';
+import { TrainingService } from '../training.service';
 
 @Component({
   selector: 'app-past-training',
   templateUrl: './past-training.component.html',
   styleUrls: ['./past-training.component.css']
 })
-export class PastTrainingComponent {
+export class PastTrainingComponent implements OnInit, AfterViewInit {
+  displayedColumns = ['date', 'name', 'duration', 'calories', 'state'];
+  dataSource = new MatTableDataSource<Exercise>();
+
+  @ViewChild(MatSort, { static: false }) sort: MatSort;
+  @ViewChild(MatPaginator, { static: false }) paginator: MatPaginator;
+
+  constructor(private trainingService: TrainingService) {}
+
+  ngOnInit(): void {
+    this.dataSource.data = this.trainingService.getCompletedOrCancelledExercises();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.sort = this.sort;
+    this.dataSource.paginator = this.paginator;
+  }
+
+  doFilter(filterValue: string) {
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 
 }
