0fcbe02be022e3af913c43ef805a649aec5950e7
Anjana Tissera <tisserapac@yahoo.com>
2023-10-28 02:19:38 +0700

    Completed upto lesson 103

--- MEAN-Stack-Guide/mean-course/backend/app.js
+++ MEAN-Stack-Guide/mean-course/backend/app.js
@@ -3,7 +3,8 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
-const postRoutes = require("./routes/posts")
+const postRoutes = require("./routes/posts");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
@@ -33,6 +34,7 @@ app.use((req, res, next) => {
     next();
   });
 
-app.use("/api/posts", postRoutes)  ;
+app.use("/api/posts", postRoutes);
+app.use("/api/user", userRoutes);
 
 module.exports = app;
---
+++ MEAN-Stack-Guide/mean-course/src/app/auth/login/login.component.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+  isLoading = false;
+
+  constructor(public authService: AuthService){
+
+  }
+
+  onLogin(form: NgForm){
+    if(form.invalid){
+      return;
+    }
+    this.authService.login(form.value.email, form.value.password);
+  }
+
+}
